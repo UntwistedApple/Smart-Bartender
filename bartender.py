5ccd22d182b6913a315d84da4563ea0c074a91fd
Philipp <untwisted@web.de>
2021-09-29 22:22:31 +0200

    LED strip removed
    Not very neat, but hopefully works
    
    Signed-off-by: Philipp <untwisted@web.de>

--- bartender.py
+++ bartender.py
@@ -271,38 +271,10 @@ class Bartender(MenuDelegate):
 		self.led.display()
 
 	def cycleLights(self):
-		t = threading.currentThread()
-		head  = 0               # Index of first 'on' pixel
-		tail  = -10             # Index of last 'off' pixel
-		color = 0xFF0000        # 'On' color (starts red)
-
-		while getattr(t, "do_run", True):
-			self.strip.setPixelColor(head, color) # Turn on 'head' pixel
-			self.strip.setPixelColor(tail, 0)     # Turn off 'tail'
-			self.strip.show()                     # Refresh strip
-			time.sleep(1.0 / 50)             # Pause 20 milliseconds (~50 fps)
-
-			head += 1                        # Advance head position
-			if(head >= self.numpixels):           # Off end of strip?
-				head    = 0              # Reset to start
-				color >>= 8              # Red->green->blue->black
-				if(color == 0): color = 0xFF0000 # If black, reset to red
-
-			tail += 1                        # Advance tail position
-			if(tail >= self.numpixels): tail = 0  # Off end? Reset
+		pass
 
 	def lightsEndingSequence(self):
-		# make lights green
-		for i in range(0, self.numpixels):
-			self.strip.setPixelColor(i, 0xFF0000)
-		self.strip.show()
-
-		time.sleep(5)
-
-		# turn lights off
-		for i in range(0, self.numpixels):
-			self.strip.setPixelColor(i, 0)
-		self.strip.show() 
+		pass
 
 	def pour(self, pin, waitTime):
 		GPIO.output(pin, GPIO.LOW)
@@ -323,10 +295,6 @@ class Bartender(MenuDelegate):
 		# cancel any button presses while the drink is being made
 		# self.stopInterrupts()
 		self.running = True
-
-		# launch a thread to control lighting
-		lightsThread = threading.Thread(target=self.cycleLights)
-		lightsThread.start()
 
 		# Parse the drink ingredients and spawn threads for pumps
 		maxTime = 0
@@ -353,13 +321,6 @@ class Bartender(MenuDelegate):
 
 		# show the main menu
 		self.menuContext.showMenu()
-
-		# stop the light thread
-		lightsThread.do_run = False
-		lightsThread.join()
-
-		# show the ending sequence lights
-		self.lightsEndingSequence()
 
 		# sleep for a couple seconds to make sure the interrupts don't get triggered
 		time.sleep(2);
